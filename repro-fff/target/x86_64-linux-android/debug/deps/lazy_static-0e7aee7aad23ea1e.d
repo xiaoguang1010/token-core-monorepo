78b5c03753618cc1bb155b3fbf44481c6bec6cc4
xiaoguang <xiaoguang101010@163.com>
2025-12-23 16:41:46 +0800

    chore: Remove the `repro-fff` project and its associated build artifacts.

--- repro-fff/target/x86_64-linux-android/debug/deps/lazy_static-0e7aee7aad23ea1e.d
+++
@@ -1,8 +0,0 @@
-/Users/xiaoguang/work/token-core-monorepo/repro-fff/target/x86_64-linux-android/debug/deps/lazy_static-0e7aee7aad23ea1e.rmeta: /Users/xiaoguang/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/lazy_static-1.5.0/src/lib.rs /Users/xiaoguang/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/lazy_static-1.5.0/src/inline_lazy.rs
-
-/Users/xiaoguang/work/token-core-monorepo/repro-fff/target/x86_64-linux-android/debug/deps/liblazy_static-0e7aee7aad23ea1e.rlib: /Users/xiaoguang/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/lazy_static-1.5.0/src/lib.rs /Users/xiaoguang/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/lazy_static-1.5.0/src/inline_lazy.rs
-
-/Users/xiaoguang/work/token-core-monorepo/repro-fff/target/x86_64-linux-android/debug/deps/lazy_static-0e7aee7aad23ea1e.d: /Users/xiaoguang/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/lazy_static-1.5.0/src/lib.rs /Users/xiaoguang/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/lazy_static-1.5.0/src/inline_lazy.rs
-
-/Users/xiaoguang/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/lazy_static-1.5.0/src/lib.rs:
-/Users/xiaoguang/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/lazy_static-1.5.0/src/inline_lazy.rs:
