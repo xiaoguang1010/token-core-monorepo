386855f2e38a2ba3644c860ea41948015094ea29
xiaoguang <xiaoguang101010@163.com>
2025-12-23 15:40:07 +0800

    feat: Add `repro-fff` project and update `fff` crate's assembly, derive macros, build system, and CI configuration.

---
+++ repro-fff/target/x86_64-linux-android/debug/deps/byteorder-84f403c98182f7c9.d
@@ -0,0 +1,8 @@
+/Users/xiaoguang/work/token-core-monorepo/repro-fff/target/x86_64-linux-android/debug/deps/byteorder-84f403c98182f7c9.rmeta: /Users/xiaoguang/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/byteorder-1.5.0/src/lib.rs /Users/xiaoguang/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/byteorder-1.5.0/src/io.rs
+
+/Users/xiaoguang/work/token-core-monorepo/repro-fff/target/x86_64-linux-android/debug/deps/libbyteorder-84f403c98182f7c9.rlib: /Users/xiaoguang/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/byteorder-1.5.0/src/lib.rs /Users/xiaoguang/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/byteorder-1.5.0/src/io.rs
+
+/Users/xiaoguang/work/token-core-monorepo/repro-fff/target/x86_64-linux-android/debug/deps/byteorder-84f403c98182f7c9.d: /Users/xiaoguang/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/byteorder-1.5.0/src/lib.rs /Users/xiaoguang/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/byteorder-1.5.0/src/io.rs
+
+/Users/xiaoguang/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/byteorder-1.5.0/src/lib.rs:
+/Users/xiaoguang/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/byteorder-1.5.0/src/io.rs:
